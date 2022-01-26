1c8e64539cdd3e3ab80408c7d8768c98ffdc1572
Mahmudul Hasan <shuvonovo@gmail.com>
2022-01-26 21:15:31 +0600

    chakraUI library added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import { Button } from '@chakra-ui/react'
 
 function App() {
   return (
     <div className="App">
-      hello world
-    </div>
+      <Button colorScheme='red'>Button</Button>    </div>
   );
 }
 
