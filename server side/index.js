4a8d9504040dffc3d0b10184d0debe73e202acca
Mahmudul Hasan <shuvonovo@gmail.com>
2022-01-27 21:56:38 +0600

    connected to mongodb

--- server side/index.js
+++ server side/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+dotenv.config()
+connectDB()
 const app = express()
-dotenv.config
+
 app.get('/', (req, res) => { res.send('api created') });
 const PORT = process.env.PORT || 4000
 app.listen(4000, console.log(`server started ${PORT}`))
