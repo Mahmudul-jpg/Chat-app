ba8d80b09e54446f66d5f88f87cfb22e78e1ba92
Mahmudul Hasan <shuvonovo@gmail.com>
2022-01-27 18:59:14 +0600

    background of homepage and signup's UI created

---
+++ client/src/components/form/SignUp.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { Input, VStack, InputGroup, InputRightElement, Button } from '@chakra-ui/react'
+import { FormControl, FormLabel } from "@chakra-ui/react"
+function SignUp() {
+    const [show, setShow] = useState(false)
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const [confirmPassword, setConfirmPassword] = useState()
+    const [picture, setPicture] = useState()
+    const handleClick = () => {
+        setShow(!show)
+    }
+    const details = (pic) => {
+
+    }
+    const submit = () => { }
+    return <VStack spacing="3px">
+        <FormControl>
+            <FormLabel textAlign='center' >Name<Input variant='filled' bg='snow' size="lg" placeholder='Enter Your Email' onChange={(event) => setName(event.target.value)} /></FormLabel>
+        </FormControl>
+
+        <FormControl>
+            <FormLabel textAlign='center'>Email address   <Input variant='filled' bg='snow' size="lg" placeholder='Enter Your Email' onChange={(event) => setEmail(event.target.value)} />
+            </FormLabel>
+        </FormControl>
+
+        <FormControl>
+            <FormLabel textAlign='center'>Password<InputGroup><Input variant='filled' bg='snow' size="lg"
+                type={show ? "text" : "password"}
+                placeholder='Enter Your Password' onChange={(event) => setEmail(event.target.value)} />
+                <InputRightElement width='4.5rem'>
+                    <Button h='1.75rem' size='sm' bgcolor='lightsteelblue' onClick={handleClick}>
+                        {show ? 'Hide' : 'Show'}
+                    </Button>
+                </InputRightElement>
+            </InputGroup></FormLabel>
+
+        </FormControl>
+
+        <FormControl>
+            <FormLabel textAlign='center'>Confirm Password<InputGroup>
+                <Input type={show ? "text" : "password"} variant='filled' bg='snow' size="lg" placeholder='Confirm Password' onChange={(event) => setConfirmPassword(event.target.value)} />
+                <InputRightElement width='4.5rem'>
+                    <Button h='1.75rem' size='sm' bgcolor='lightsteelblue' onClick={handleClick}>
+                        {show ? 'Hide' : 'Show'}
+                    </Button>
+                </InputRightElement>
+            </InputGroup></FormLabel>
+
+        </FormControl>
+
+        <FormControl id='pic'>
+            <FormLabel textAlign='center'>Upload Your Picture<Input type="file" variant='filled' bg='snow' placeholder='Upload Your Picture' accept='image/*' onChange={(event) => details(event.target.files[0])} size='lg' /></FormLabel>
+
+        </FormControl>
+
+        <Button
+            size='lg'
+            height='48px'
+            width='480px'
+            border='2px'
+            borderColor='green.500'
+            bgColor='steelblue'
+            onClick={submit}
+        >
+            Sign Up
+        </Button>
+    </VStack>;
+}
+
+export default SignUp;
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,50 @@
 import React from 'react';
-
+import { Container, Box, Text } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import Login from '../components/form/Login'
+import SignUp from '../components/form/SignUp'
 function HomePage() {
-    return (<div>Home Page</div>);
+    return (<Container centerContent maxW='xl'><Box
+        display="flex"
+        justifyContent="center"
+        padding={3}
+        bg={"lavender"}
+        width="100%"
+        margin="10px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+    >
+        <Text color="blue"
+            fontWeight="bold"
+            fontSize='50px'
+            fontFamily='work sans'
+        >Chat with your friend</Text>
+    </Box>
+        <Box display="flex"
+            justifyContent="center"
+            padding={3}
+            bg={"lavender"}
+            width="100%"
+            margin="20px 0 15px 0"
+            borderRadius="lg"
+            borderWidth="1px">
+            <Tabs isFitted variant='enclosed'>
+                <TabList mb='0.1em'>
+                    <Tab bgColor={"skyblue"} color={"white"} margin={"5px"}>LOGIN</Tab>
+                    <Tab bgColor={"lightblue"} color={"white"} margin={"5px"}>SIGNUP</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+                        <Login />
+
+                    </TabPanel>
+                    <TabPanel>
+                        <SignUp />
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </Box>
+    </Container>);
 };
 
 export default HomePage;
