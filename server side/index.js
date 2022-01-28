dd2784e8396487dd7bf11d16194f24b1bdadf2f1
Mahmudul Hasan <shuvonovo@gmail.com>
2022-01-28 21:37:33 +0600

    added error handler to server side

--- server side/index.js
+++ server side/index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const userRoutes = require('./routes/userRoutes')
+const { notFound, errorHandler } = require('./middleware/error')
 dotenv.config()
 connectDB()
 const app = express()
 app.use(express.json())
 // app.get('/', (req, res) => { res.send('api created') });
 app.use("/api/user", userRoutes)
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 4000
 app.listen(4000, console.log(`server started ${PORT}`))
