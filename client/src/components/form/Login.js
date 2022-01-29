acc438952314d49a77ff55ae257c01237f8abcc5
Mahmudul Hasan <shuvonovo@gmail.com>
2022-01-29 23:36:59 +0600

    login functionality added

--- client/src/components/form/Login.js
+++ client/src/components/form/Login.js
@@ -3,15 +3,65 @@ import { Button } from "@chakra-ui/button";
 import { FormControl, FormLabel } from "@chakra-ui/form-control";
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
 import { VStack } from "@chakra-ui/layout";
+import { useToast } from '@chakra-ui/react'
+import axios from "axios"
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
     const [show, setShow] = useState(false)
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
     const handleClick = () => setShow(!show);
-
-
-    const submit = () => { }
+    const [loading, setLoading] = useState(false)
+    const toast = useToast()
+    const navigate = useNavigate();
+    
+    const submit = async () => {
+        setLoading(true)
+        if (!email || !password) {
+            toast({
+                title: "Please fill all the fields",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            })
+            setLoading(false)
+            return
+        }
+        try {
+            const config = {
+                headers: {
+                    "Content-type": "application/json",
+                }
+            }
+            const {data} = await axios.post(
+                "/api/user/login",
+                {email,password},
+                config
+            )
+            toast({
+                title: "Login Successful",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            })
+            localStorage.setItem('userInfo', JSON.stringify(data))
+            setLoading(false)
+            navigate.push("/chats")
+        } catch (error) {
+            toast({
+                title: "Error Occured",
+                description: error.response.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            })
+            setLoading(false)
+        }
+    }
     return (
         <VStack spacing="10px">
             <FormControl id="email" isRequired>
@@ -47,7 +97,8 @@ function Login() {
                 colorScheme="blue"
                 width="100%"
                 style={{ marginTop: 15 }}
-
+                onClick={submit}
+                isLoading={loading}
             >
                 Login
             </Button>
