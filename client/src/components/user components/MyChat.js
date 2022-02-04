9e3cc4af424ae7593af9690763c298439a40417b
Mahmudul Hasan <shuvonovo@gmail.com>
2022-02-04 22:27:49 +0600

    Sidebar,MyChat and ChatBox components created and ded little bit styling in those components

---
+++ client/src/components/user components/MyChat.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyChat = () => {
+    return <div>my chat</div>;
+};
+
+export default MyChat;
--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import SideBar from '../components/user components/SideBar';
+import MyChat from '../components/user components/MyChat'
+import ChatBox from '../components/user components/ChatBox'
 import { ChatState } from '../context/ChatProvider';
-
+import {Box} from "@chakra-ui/layout"
 function ChatPage() {
     const { user } = ChatState()
 
-    return (<div>
+    return (<div style={{width:'100%'}}>
         {user && <SideBar />}
 
+<Box d='flex'
+     justifyContent='space-between'
+     w='100%'
+     h='91vh'
+     p='10px'
+     >
+{user&& <MyChat/>}
+{user&& <ChatBox/>} 
 
+</Box>
     </div>);
 };
 
