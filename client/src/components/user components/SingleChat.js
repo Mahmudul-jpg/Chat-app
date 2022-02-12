ae8bc9a50b100d9f1c30eca5e16560d9a6460eb0
Mahmudul Hasan <shuvonovo@gmail.com>
2022-02-12 12:09:20 +0600

    created send Message UI

--- client/src/components/user components/SingleChat.js
+++ client/src/components/user components/SingleChat.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ChatState } from '../../context/ChatProvider'
-import { Box, Text, IconButton } from '@chakra-ui/react'
+import { Box, Text, IconButton, Spinner, FormControl, Input } from '@chakra-ui/react'
 import { ArrowBackIcon } from '@chakra-ui/icons'
 import { getSender, getSenderFull } from '../../config/ChatLogics'
 import Profile from './Profile'
 import UpdateGroup from './UpdateGroup'
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
+    const [loading, setLoading] = useState()
+    const [message, setMessage] = useState()
     const { user, selectedChat, setSelectedChat } = ChatState()
+
+    const sendMessage = () => {
+
+    }
+    const typingHandler = () => {
+
+    }
     return <>{
         selectedChat ? (<>
             <Text fontSize={{ base: "28px", md: "30px" }}
@@ -27,7 +36,19 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                 </>)}
             </Text>
             <Box d='flex' flexDir='column' justifyContent='flex-end' p={3} bg="#87CEEB" w='100%' h='100%' borderRadius='lg' overflowY='hidden'>
+                {!loading ? (<Spinner
+                    size="xl"
+                    w="20"
+                    h="20"
+                    alignSelf="center"
+                    margin="auto"
+                />) : (<>
 
+
+                </>)}
+                <FormControl onKeyDown={sendMessage}>
+                    <Input placeholder="Enter your message" bg="whitesmoke" p='30px' variant="filled" onChange={typingHandler} />
+                </FormControl>
             </Box>
         </>) : (<Box d="flex" alignItems="center" justifyContent="center" h="100%">
             <Text fontSize="3xl" pb={3} fontFamily="Work sans">
